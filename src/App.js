d9d8769aaea84a1ebfc29f1ae272bc0dc0219002
anshulrts <anshul.rts@gmail.com>
2020-06-14 11:12:50 +0530

    Using Arrow Fucntion instead of Named Function - In Arrow function, you can have either a concise body with no return   statement and curly braces. return is implicit here - Otherwise, you have a block body, with braces and return

--- src/App.js
+++ src/App.js
@@ -27,8 +27,7 @@ class App extends Component {
     return (
       <div className="App">
         {
-          list.map(function(item){
-            return(
+          list.map(item => 
                   <div key={item.objectID}>
                     <span>
                       <a href={item.url}>{item.title}</a>
@@ -37,8 +36,7 @@ class App extends Component {
                     <span>{item.num_comments}</span>
                     <span>{item.points}</span>
                   </div>
-            );
-          })}
+            )}
       </div>
     );
   }
