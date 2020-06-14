0ed85f17d5ea3e46fca082de0983fea7956407a9
anshulrts <anshul.rts@gmail.com>
2020-06-14 13:23:51 +0530

    Adding a search functionality among titles

--- src/App.js
+++ src/App.js
@@ -21,6 +21,9 @@ const list = [
   },
 ];
 
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
 
   constructor(props){
@@ -28,6 +31,7 @@ class App extends Component {
 
     this.state = {
       list,
+      searchTerm: '',
     };
   }
   
@@ -36,28 +40,39 @@ class App extends Component {
     this.setState({list: updatedList});
   }
 
+  onSearchChange = event => {
+    this.setState({searchTerm: event.target.value})
+  }
+
   render() {
     return (
       <div className="App">
+        <form>
+          <input
+            type="text"
+            onChange={this.onSearchChange}
+          />
+        </form>
         {
-          this.state.list.map(item => 
-                  <div key={item.objectID}>
-                    <span>
-                      <a href={item.url}>{item.title}</a>
-                    </span>
-                    <span>{item.author}</span>
-                    <span>{item.num_comments}</span>
-                    <span>{item.points}</span>
-                    <span>
-                      <button
-                        onClick = {() => this.onDismiss(item.objectID)}
-                        type = "button"
-                      >
-                        Dismiss
-                      </button>
-                    </span>
-                  </div>
-            )}
+          this.state.list.filter(isSearched(this.state.searchTerm)).map(item => 
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button
+                  onClick = {() => this.onDismiss(item.objectID)}
+                  type = "button"
+                >
+                  Dismiss
+                </button>
+              </span>
+            </div>
+          )
+        }
       </div>
     );
   }
